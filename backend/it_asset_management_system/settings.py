9adabb239c85d49f4c575213e0054840d5be3dc0
Sayed <sayedrayhan10@gmail.com>
2026-02-08 10:10:35 +0600

    Make the Products, Vendors and Departments component and connenct with backend

--- backend/it_asset_management_system/settings.py
+++ backend/it_asset_management_system/settings.py
@@ -42,9 +42,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'autoslug',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +55,17 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
+CORS_ALLOW_ALL_ORIGINS = True 
+CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:8000",
+]
+
+
 
 ROOT_URLCONF = 'it_asset_management_system.urls'
 
